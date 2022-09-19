2fcd41ad8381b3e20410a785dbcf7bcd36d66094
Anton Nakov <anton.nakov@gmail.com>
2022-09-19 19:07:38 +0200

    Update jsfile.js

--- files/src/jsfile.js
+++ files/src/jsfile.js
@@ -1,24 +1,13 @@
 
 /* Institutions ***************************************************************/
 
-$('a[id=cemfi]'        ).attr('href','https://www.cemfi.es/');
-$('a[id=cunef]'        ).attr('href','https://www.cunef.edu/en/');
-$('a[id=airef]'        ).attr('href','https://www.airef.es/en/');
-$('a[id=ucm]'          ).attr('href','https://economicasyempresariales.ucm.es');
+$('a[id=upf]'        ).attr('href','https://www.upf.edu/');
+$('a[id=ecb]'        ).attr('href','https://www.ecb.int');
 
 /* People *********************************************************************/
 
 $('a[id=jim]'          ).attr('href','https://sites.google.com/site/jimcostain/');
-$('a[id=anton]'        ).attr('href','https://sites.google.com/site/antonnakov/');
-$('a[id=josep]'        ).attr('href','https://www.cemfi.es/~pijoan/Welcome.html');
-$('a[id=nezih]'        ).attr('href','https://www.cemfi.es/~guner/');
-$('a[id=jc]'           ).attr('href','https://jcruizgarcia.com/');
-$('a[id=javi]'         ).attr('href','https://sites.google.com/view/javierlopezsegovia/home?authuser=0');
-$('a[id=enriquemoral]' ).attr('href','https://moralbenito.weebly.com/');
-$('a[id=tonibraun]'    ).attr('href','https://www.frbatlanta.org/research/economists/braun-anton.aspx');
-$('a[id=juanfranjim]'  ).attr('href','https://www.bde.es/investigador/en/menu/people/research_staff_a/Jimeno_Serrano__Juan_Francisco.html');
-$('a[id=davidlopez]'   ).attr('href','https://sites.google.com/site/davidlopezrodriguezphd/');
-$('a[id=almunia]'      ).attr('href','https://malmunia.github.io/');
+$('a[id=borja]'        ).attr('href','https://www.cunef.edu/en/claustro/petit-zarzalejos-borja-maria/');
 
 /* Defined links options *******************************************************/
 
