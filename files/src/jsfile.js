f3cc64d59e629ccdd8e9a911b33d9412c79d61b8
Anton Nakov <anton.nakov@gmail.com>
2025-11-24 11:21:53 +0100

    corrections

--- files/src/jsfile.js
+++ files/src/jsfile.js
@@ -1,66 +1,65 @@
-
 /* Institutions ***************************************************************/
 
-$('a[id=upf]'        ).attr('href','https://www.upf.edu/');
-$('a[id=ecb]'        ).attr('href','https://www.ecb.int');
+$('a[id=upf]').attr('href','https://www.upf.edu/');
+$('a[id=ecb]').attr('href','https://www.ecb.int');
 
 /* People *********************************************************************/
 
-$('a[id=jim]'          ).attr('href','https://sites.google.com/site/jimcostain/');
-$('a[id=borja]'        ).attr('href','https://borjapetit.github.io/');
-$('a[id=peter]'        ).attr('href','https://sites.google.com/site/pkaradi696/');
-$('a[id=galo]'        ).attr('href','https://www.galonuno.com/');
-$('a[id=carlos]'        ).attr('href','https://sites.google.com/site/carlosthomaseconweb/Home/');
-$('a[id=max]'        ).attr('href','https://maxgillman.com/');
-$('a[id=billi]'        ).attr('href','http://www.rmbilli.com/');
-$('a[id=gali]'        ).attr('href','https://crei.cat/people/gali/');
-$('a[id=weber]'        ).attr('href','https://www.bundesbank.de/en/henning-weber');
-$('a[id=mishel]'        ).attr('href','https://sites.google.com/view/mishel-ghassibe');
-$('a[id=ernesto]'        ).attr('href','https://sites.google.com/site/pastenernesto/');
-$('a[id=dominik]'        ).attr('href','https://sites.google.com/site/dominikthaler/');
+$('a[id=jim]').attr('href','https://sites.google.com/site/jimcostain/');
+$('a[id=borja]').attr('href','https://borjapetit.github.io/');
+$('a[id=peter]').attr('href','https://sites.google.com/site/pkaradi696/');
+$('a[id=galo]').attr('href','https://www.galonuno.com/');
+$('a[id=carlos]').attr('href','https://sites.google.com/site/carlosthomaseconweb/Home/');
+$('a[id=max]').attr('href','https://maxgillman.com/');
+$('a[id=billi]').attr('href','http://www.rmbilli.com/');
+$('a[id=gali]').attr('href','https://crei.cat/people/gali/');
+$('a[id=weber]').attr('href','https://www.bundesbank.de/en/henning-weber');
+$('a[id=mishel]').attr('href','https://sites.google.com/view/mishel-ghassibe');
+$('a[id=ernesto]').attr('href','https://sites.google.com/site/pastenernesto/');
+$('a[id=dominik]').attr('href','https://sites.google.com/site/dominikthaler/');
 
 
 /* Defined links options *******************************************************/
 
-$('a'                  ).attr('target','_blank');
-$('a[class=simple]'    ).attr('target','_self');
-$('a[class=top]'       ).attr('target','_self');
-$('a[class=down]'      ).attr('target','_self');
-$('a[class=toc]'       ).attr('target','_self');
-$('a[class=toca]'      ).attr('target','_self');
-$('a[class=toccv]'     ).attr('target','_blank');
-$('a[class=title]'     ).attr('target','_self');
+$('a').attr('target','_blank');
+$('a[class=simple]').attr('target','_self');
+$('a[class=top]' ).attr('target','_self');
+$('a[class=down]').attr('target','_self');
+$('a[class=toc]' ).attr('target','_self');
+$('a[class=toca]').attr('target','_self');
+$('a[class=toccv]' ).attr('target','_blank');
+$('a[class=title]' ).attr('target','_self');
 
 /* Show/Hide functions *********************************************************/
 
 $(document).on("click", "a.plus", function(event){
-    event.preventDefault();
-    $(this).next('.hidden').slideDown('slow');
-    $(this).attr('class', 'minus');
+  event.preventDefault();
+  $(this).next('.hidden').slideDown('slow');
+  $(this).attr('class', 'minus');
 });
 
 $(document).on("click", "a.minus", function(event){
-    event.preventDefault();
-    $(this).next('.hidden').slideUp('slow');
-    $(this).attr('class', 'plus');
+  event.preventDefault();
+  $(this).next('.hidden').slideUp('slow');
+  $(this).attr('class', 'plus');
 });
 
 $(document).on("click", "a.more", function(event){
-    event.preventDefault();
-    $(this).attr('class', 'less');
+  event.preventDefault();
+  $(this).attr('class', 'less');
 });
 
 $(document).on("click", "a.less", function(event){
-    event.preventDefault();
-    $(this).attr('class', 'more');
+  event.preventDefault();
+  $(this).attr('class', 'more');
 });
 
 function showhide(xx) {
   var x = document.getElementById(xx)
   if (x.style.display === "none") {
-    $(x).slideDown('slow');
+  $(x).slideDown('slow');
   } else {
-    $(x).slideUp('slow');
+  $(x).slideUp('slow');
   }
 }
 
